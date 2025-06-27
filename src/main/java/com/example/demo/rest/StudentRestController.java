49f54d0f5dafa2b3b994f4b96dbacaef5059728e
rzzza_rehan <rhn.raza121@gmail.com>
2025-06-28 00:43:06 +0530

    Global Exception Handling

--- src/main/java/com/example/demo/rest/StudentRestController.java
+++ src/main/java/com/example/demo/rest/StudentRestController.java
@@ -42,35 +42,4 @@ public class StudentRestController {
 
         return students.get(studentId);
     }
-
-    // Add an exceptional handler using @ExceptionalHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-
-        // create a StudentErrorResponse
-
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // add another exceptional handler ... to catch any exception (catch all)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-        // create a StudentErrorResponse
-
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
